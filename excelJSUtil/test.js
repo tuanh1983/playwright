6c7af88d11de8149435a28b8d267785f2bb47903
tuanh1983 <122734267+tuanh1983@users.noreply.github.com>
2024-12-12 12:26:25 +0700

    add: implement readExcelForScan function to locate and update specific cell in Excel file

--- excelJSUtil/test.js
+++ excelJSUtil/test.js
@@ -17,4 +17,27 @@ async function readExcel() {
   });
   await workbook.xlsx.writeFile("test.xlsx");
 }
-readExcel();
+async function readExcelForScan() {
+  let output = { row: -1, column: -1 };
+  await workbook.xlsx.readFile("test.xlsx");
+  const worksheet = workbook.getWorksheet("Sheet1");
+
+  if (!worksheet) {
+    console.error("Worksheet 'Sheet1' not found");
+    return;
+  }
+
+  worksheet.eachRow((row, rowNumber) => {
+    row.eachCell((cell, colNumber) => {
+      console.log(`Cell ${rowNumber}, ${colNumber}: ${cell.value}`);
+      if (cell.value === "scan") {
+        output.row = rowNumber;
+        output.column = colNumber;
+      }
+    });
+  });
+  var cell = worksheet.getCell(output.row, output.column);
+  cell.value = cell.value + " - updated";
+  await workbook.xlsx.writeFile("test.xlsx");
+}
+readExcelForScan();
