607d3322205132a772802dfb8492db7232947d08
tuanh1983 <tuanh.ti@gmail.com>
2024-12-13 12:00:21 +0700

    add: update login credentials in loginData.json; refactor POManager for improved page object instantiation; enhance TestCase01 to utilize dynamic test data

--- pageobjects/POManager.js
+++ pageobjects/POManager.js
@@ -1,16 +1,16 @@
-require("./LoginPage");
-require("./ProductPage");
+const { LoginPage } = require("./LoginPage");
+const { ProductPage } = require("./ProductPage");
 class POManager {
   constructor(page) {
     this.page = page;
-    this.loingPage = new LoginPage();
-    this.PrtoductPage = new ProductPage();
+    this.loingPage = new LoginPage(page);
+    this.productPage = new ProductPage(page);
   }
   getLoginPage() {
     return this.loingPage;
   }
   getProductPage() {
-    return this.PrtoductPage;
+    return this.productPage;
   }
 }
 module.exports = { POManager };
--- tests/TestCase/TestCase01.spec.js
+++ tests/TestCase/TestCase01.spec.js
@@ -1,24 +1,25 @@
 const { test, expect } = require("@playwright/test");
 const { POManager } = require("../../pageobjects/POManager");
+const loginData = require("../../data/loginData.json");
+const testData = require("../../data/testDataTC01.json");
 let webContext;
 const fakePayload = { data: [], message: "No Product in Cart" };
-const userName = "rahulshetty@gmail.com";
-const password = "Iamking@000";
-const searchProduct = "ZARA COAT 3";
-
+const userName = loginData.userName;
+const password = loginData.password;
+const searchProduct = testData.searchProduct;
 test.beforeAll(async ({ browser }) => {
-  // Đăng nhập và lưu trạng thái
   const page = await browser.newPage();
   const context = await browser.newContext();
+  // Đăng nhập và lưu trạng thái
   const poManager = new POManager(page);
-  const loginPage = new poManager.getLoginPage();
+  const loginPage = poManager.getLoginPage();
   await loginPage.goTo();
   await loginPage.login(userName, password);
   await page.waitForLoadState("networkidle");
   await context.storageState({ path: "state.json" });
   // Tạo webContext từ trạng thái đã lưu
   webContext = await browser.newContext({ storageState: "state.json" });
-  const productPage = new poManager.getProductPage();
+  const productPage = poManager.getProductPage();
   await productPage.searchProduct(searchProduct);
   await productPage.goToCart();
 });
