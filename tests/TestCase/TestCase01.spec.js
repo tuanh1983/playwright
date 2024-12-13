0ff569b83c61a7cfd4a023092dcb5755b7a00f26
tuanh1983 <tuanh.ti@gmail.com>
2024-12-13 12:18:37 +0700

    add: update testDataTC01.json to support multiple search products; refactor TestCase01 to iterate over test data for login and product search

--- tests/TestCase/TestCase01.spec.js
+++ tests/TestCase/TestCase01.spec.js
@@ -7,20 +7,17 @@ const fakePayload = { data: [], message: "No Product in Cart" };
 const userName = loginData.userName;
 const password = loginData.password;
 const searchProduct = testData.searchProduct;
-test.beforeAll(async ({ browser }) => {
-  const page = await browser.newPage();
-  const context = await browser.newContext();
-  // Đăng nhập và lưu trạng thái
-  const poManager = new POManager(page);
-  const loginPage = poManager.getLoginPage();
-  await loginPage.goTo();
-  await loginPage.login(userName, password);
-  await page.waitForLoadState("networkidle");
-  await context.storageState({ path: "state.json" });
-  // Tạo webContext từ trạng thái đã lưu
-  webContext = await browser.newContext({ storageState: "state.json" });
-  const productPage = poManager.getProductPage();
-  await productPage.searchProduct(searchProduct);
-  await productPage.goToCart();
-});
-test("Login Test 01", async () => {});
+
+for (const data of testData) {
+  test(`Login Test 01 ${data.searchProduct}`, async ({ page }) => {
+    // Đăng nhập và lưu trạng thái
+    const poManager = new POManager(page);
+    const loginPage = poManager.getLoginPage();
+    await loginPage.goTo();
+    await loginPage.login(userName, password);
+    await page.waitForLoadState("networkidle");
+    const productPage = poManager.getProductPage();
+    await productPage.searchProduct(data.searchProduct);
+    await productPage.goToCart();
+  });
+}
